f601323a649d53fffc43f761d99697818aee316d
Danny Tseng <github email address>
2019-06-17 15:26:06 -0700

    created user and reminder models and controllers

--- noteminder/db/schema.rb
+++ noteminder/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_204232) do
+ActiveRecord::Schema.define(version: 2019_06_17_221851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,27 @@ ActiveRecord::Schema.define(version: 2019_06_17_204232) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
+  create_table "reminders", force: :cascade do |t|
+    t.bigint "note_id"
+    t.string "priority"
+    t.string "location"
+    t.string "date"
+    t.string "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["note_id"], name: "index_reminders_on_note_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "notes", "users"
+  add_foreign_key "reminders", "notes"
 end
